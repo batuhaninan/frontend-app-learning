c5400d68e4da28a0034f3f9fe20fb915e23dd8c9
crtmneric <s3rkanodemir@gmail.com>
2023-06-13 11:10:42 +0300

    Changes styles

---
+++ src/tab-page/ModifiedHeader.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { render } from 'react-dom';
+import { LearningHeader as Header } from '@edx/frontend-component-header';
+
+const replaceHref = () => {
+  useEffect(() => {
+    const links = document.querySelectorAll('header a');
+
+    links.forEach((link) => {
+      link.setAttribute('href', 'https://pupilica.com');
+    });
+  }, []);
+
+  return null; // Return null as we don't need to render anything
+};
+
+const HeaderWrapper = ({ courseOrg, courseNumber, courseTitle, showUserDropdown }) => {
+  replaceHref(); // Call the replaceHref function to modify the href attributes
+
+  return (
+    <Header
+      courseOrg={courseOrg}
+      courseNumber={courseNumber}
+      courseTitle={courseTitle}
+      showUserDropdown={showUserDropdown}
+    />
+  );
+};
