975c93f35e38a3c1783256e4a0866bfee0a009e0
crtmneric <s3rkanodemir@gmail.com>
2023-08-07 11:31:30 +0300

    Changes styles

--- src/course-home/outline-tab/widgets/CourseTools.jsx
+++ src/course-home/outline-tab/widgets/CourseTools.jsx
@@ -66,7 +66,7 @@ function CourseTools({ intl }) {
       <h2 className="h4">{intl.formatMessage(messages.tools)}</h2>
       <ul className="list-unstyled">
         {courseTools.map((courseTool) => (
-          courseTool.analyticsId === "edx.bookmarks" ? (
+          courseTool.analyticsId === "edx.bookmarks" && courseTool.analyticsId !== null ? (
             <li key={courseTool.analyticsId} className="small">
               <a href={courseTool.url} onClick={() => logClick(courseTool.analyticsId)}>
                 <FontAwesomeIcon icon={renderIcon(courseTool.analyticsId)} className="mr-2" fixedWidth />
