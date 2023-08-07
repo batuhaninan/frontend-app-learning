268d54a51bc16aa2b7b4db0e96e72d337dbebca0
crtmneric <s3rkanodemir@gmail.com>
2023-08-07 11:18:00 +0300

    Changes styles

--- src/course-home/outline-tab/widgets/CourseTools.jsx
+++ src/course-home/outline-tab/widgets/CourseTools.jsx
@@ -66,12 +66,14 @@ function CourseTools({ intl }) {
       <h2 className="h4">{intl.formatMessage(messages.tools)}</h2>
       <ul className="list-unstyled">
         {courseTools.map((courseTool) => (
-          <li key={courseTool.analyticsId} className="small">
-            <a href={courseTool.url} onClick={() => logClick(courseTool.analyticsId)}>
-              <FontAwesomeIcon icon={renderIcon(courseTool.analyticsId)} className="mr-2" fixedWidth />
-              {courseTool.title}-{courseTool.analyticsId}
-            </a>
-          </li>
+          courseTool.analyticsId === "edx.bookmarks" ? (
+            <li key={courseTool.analyticsId} className="small">
+              <a href={courseTool.url} onClick={() => logClick(courseTool.analyticsId)}>
+                <FontAwesomeIcon icon={renderIcon(courseTool.analyticsId)} className="mr-2" fixedWidth />
+                {courseTool.title}
+              </a>
+            </li>
+          ) : null
         ))}
         <li className="small" id="courseHome-launchTourLink">
           <LaunchCourseHomeTourButton />
