f130cca8c6676939fa13e9add2316826fea3bc8c
crtmneric <s3rkanodemir@gmail.com>
2023-08-28 09:07:47 +0300

    Changes styles

--- src/course-home/outline-tab/OutlineTab.jsx
+++ src/course-home/outline-tab/OutlineTab.jsx
@@ -9,13 +9,12 @@ import { Button } from '@edx/paragon';
 import { AlertList } from '../../generic/user-messages';
 import { faCheckCircle as fasCheckCircle, faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons';
 import { Icon, IconButton } from '@edx/paragon';
-
 import CourseDates from './widgets/CourseDates';
 import CourseHandouts from './widgets/CourseHandouts';
 import StartOrResumeCourseCard from './widgets/StartOrResumeCourseCard';
 import WeeklyLearningGoalCard from './widgets/WeeklyLearningGoalCard';
 import CourseTools from './widgets/CourseTools';
-import { fetchOutlineTab } from '../data';
+import { fetchOutlineTab, fetchProgressTab } from '../data';
 import messages from './messages';
 import Section from './Section';
 import ShiftDatesAlert from '../suggested-schedule-messaging/ShiftDatesAlert';
@@ -31,14 +30,19 @@ import WelcomeMessage from './widgets/WelcomeMessage';
 import ProctoringInfoPanel from './widgets/ProctoringInfoPanel';
 import AccountActivationAlert from '../../alerts/logistration-alert/AccountActivationAlert';
 import CourseCompletion from '../progress-tab/course-completion/CourseCompletion';
+import OutlineProgress from './Outlineprogress'
 
 /** [MM-P2P] Experiment */
 import { initHomeMMP2P, MMP2PFlyover } from '../../experiments/mm-p2p';
 
 function OutlineTab({ intl }) {
+
+
+
   const {
     courseId,
     proctoringPanelStatus,
+    targetUserId,
   } = useSelector(state => state.courseHome);
 
   const {
@@ -130,6 +134,7 @@ function OutlineTab({ intl }) {
     }
   }, [location.search]);
 
+
   return (
     <>
       <div data-learner-type={learnerType} className="row w-100 mx-0 my-3 justify-content-between">
@@ -158,6 +163,7 @@ function OutlineTab({ intl }) {
             </>
           )}
           <WelcomeMessage courseId={courseId} />
+          <OutlineProgress courseId={courseId}/>
           {rootCourseId && (
             <>
               <div className="row w-100 m-0 mb-3 justify-content-end">
---
+++ src/course-home/outline-tab/Outlineprogress.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import { useModel } from '../../generic/model-store';
+import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
+
+// Define your new component
+function OutlineProgress({ courseId }) {
+    const [progressData, setProgressData] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                // Define the URL with the actual courseId
+                const url = `https://courses-test.pupilica.com/api/course_home/progress/${courseId}`;
+
+                // Make the GET request using an asynchronous function
+                const response = await getAuthenticatedHttpClient().get(url);
+
+                // Set the fetched data in state
+                setProgressData(response.data.completion_summary);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+
+        // Call the async function
+        fetchData();
+    }, [courseId]);
+
+
+    // Check if completionSummary is defined before accessing its properties
+    const completeCount = progressData?.complete_count || 0;
+    const incompleteCount = progressData?.incomplete_count || 0;
+    const lockedCount = progressData?.locked_count || 0;
+    const numTotalUnits = completeCount + incompleteCount + lockedCount;
+    const completePercentage = completeCount ? Number(((completeCount / numTotalUnits) * 100).toFixed(0)) : 0;
+    const progressBarStyle = {
+        width: `${completePercentage}%`,
+        height: '10px', // Adjust the height
+        background: `linear-gradient(to right, #aaf3a8, #57cc99)`,
+        borderRadius: '10px',
+        transition: 'width 0.5s ease-in-out',
+        position: 'relative',
+    };
+    const containerStyle = {
+        width: `100%`,
+        height: '10px', // Adjust the height
+        backgroundColor: 'lightgray',
+        borderRadius: '10px'
+
+    };
+    const labelStyle = {
+        fontWeight: 'bold',
+    };
+    // Render the component once completionSummary is available
+    return (
+        <div>
+            <p style={labelStyle}>Tamamlama Oranı: %{completePercentage}</p>
+            {/* Add your JSX code here that uses completionSummary */}
+            {/* Render the progress bar */}
+            <div style={containerStyle}>
+                <div style={progressBarStyle}></div>
+            </div>
+        </div>
+    );
+}
+
+export default OutlineProgress;
+
+
+
+
+
